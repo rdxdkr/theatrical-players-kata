0e772ee9983055aa73cd96374fc87fe3099269f0
rdxdkr <davide.franchinetti@gmail.com>
2024-07-05 23:38:35 +0200

    renamed argument

--- src/main/java/theatricalplays/StatementPrinter.java
+++ src/main/java/theatricalplays/StatementPrinter.java
@@ -36,26 +36,26 @@ public class StatementPrinter {
         return plays.get(aPerformance.playID);
     }
 
-    private int amountFor(Performance performance) {
+    private int amountFor(Performance aPerformance) {
         var result = 0;
 
-        switch (playFor(performance).type) {
+        switch (playFor(aPerformance).type) {
             case "tragedy":
                 result = 40000;
 
-                if (performance.audience > 30) {
-                    result += 1000 * (performance.audience - 30);
+                if (aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience - 30);
                 }
 
                 break;
             case "comedy":
                 result = 30000;
 
-                if (performance.audience > 20) {
-                    result += 10000 + 500 * (performance.audience - 20);
+                if (aPerformance.audience > 20) {
+                    result += 10000 + 500 * (aPerformance.audience - 20);
                 }
 
-                result += 300 * performance.audience;
+                result += 300 * aPerformance.audience;
                 break;
             default:
                 throw new Error("unknown type: ${play.type}");
